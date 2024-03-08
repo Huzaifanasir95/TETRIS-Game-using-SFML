f33f2c5e22f5a9944d82ac9a36d782ad68f74066
Huzaifa Nasir <114720031+Huzaifanasir95@users.noreply.github.com>
2024-03-08 16:46:21 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,218 +1,170 @@
-/
-#include<iostream>
-#include<fstream>
-#include <sstream>
-#include <fstream>
+/* PROGRAMMING FUNDAMENTAL'S PROJECT FOR FALL 2022 BS(CS)
+ * You don't need to change anything in the driver code/main function except the part(s) where instructed.
+ * You are expected to define your functionalities in the "functionality.h" header and call them here in the instructed part(s).
+ * Load the images into the textures objects from img directory.
+ * Don't make any changes to the "utils.h" header file except for changing the coordinate of screen or global variables of the game.
+ * SUGGESTION: If you want to make changes in "utils.h", do it once you complete rest of the functionalities.
+ * TIP: You can study SFML for drawing scenes, sprites and window.
+ * */
+
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <time.h>
 #include "utils.h"
 #include "pieces.h"
 #include "functionality.h"
 using namespace sf;
-int main() {
+
+int main(){
+
+display_menu();
+
 
 
     srand(time(0));
-    Texture obj1, obj2, obj3, obj4;
-
-
-
-
-
     RenderWindow window(VideoMode(320, 480), title);
-
-    obj3.loadFromFile("img/frame.png");
-    obj1.loadFromFile("img/tiles.png");
-    obj2.loadFromFile("img/background.png");
+    Texture obj1, obj2, obj3;
     /*
         Load "frames.png","tiles.png" and "background.png" for Texture objects
     */
-    Sprite bomb(obj1), sprite(obj1), background(obj2), frame(obj3);
-    float timer = 0;
-    int level = 1;
-    float delay = 0;
-    int score = 0;
-    bool gamestate = false;
-    menu(timer, menubg, level, delay, score);			//menu ftn
-    Music music;						//creates music file to be played
-
-    music.setLoop(true);
-    music.play();
-
-
-
-    int delta_x = 0, colorNum = 1;
-    int var = 10;
-    bool lineclear = 0, rotate = 0;
-    bool bombbool = false;
+     obj1.loadFromFile("img/tiles.png");
+    obj2.loadFromFile("img/background.png");
+    obj3.loadFromFile("img/frame.png");
+    Sprite sprite(obj1), background(obj2), frame(obj3);
+    int delta_x=0, colorNum=1;
+    float timer=0, delay=0.3;
+    bool rotate=0;
     Clock clock;
- 
-    while (window.isOpen()) {
+     bool pausegame = false;
+       Font font;
+    font.loadFromFile("Pencil Child.otf");
+    RectangleShape pauseScreen(Vector2f(200, 100));
+    pauseScreen.setFillColor(Color(0, 0, 0, 128));
+    pauseScreen.setPosition(90, 150);
 
+    Text pausedText("Game Paused", font, 32);
+    pausedText.setPosition(90, 150);
+    
+    Text resumeText("Resume Game", font, 24);
+    resumeText.setPosition(110, 190);
+    resumeText.setFillColor(sf::Color::Red);
+
+    Text quitText("Quit Game", font, 24);
+    quitText.setPosition(120, 240);
+
+   while (window.isOpen()){
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
-        timer += time;
+        timer+=time;
+rotate=false;
 
-   
-
-         //---Event Listening Part---//
-        Event e, a;
-        while (window.pollEvent(e)) {                     //Event is occurring - until the game is in running state
-            if (e.type == Event::Closed)               	 //If cross/close is clicked/pressed
+  Event e;
+       /* while (window.pollEvent(e)) {
+            if (e.type == Event::Closed) {
                 window.close();
-            //Opened window disposes
-            if (e.type == Event::KeyPressed && bombbool == false) {             //If any other key (not cross) is pressed
-                if (Keyboard::isKeyPressed(Keyboard::Down))   //Just another way to detect key presses without event listener
-                { //delay=softdrop(level, delay);
-                    delay = 0.05;  //If DOWN key is pressed, goes to soft drop 
+            }
+            else if (e.type == Event::KeyPressed) {
+                if (e.key.code == Keyboard::Escape) {
+                    pausegame = true;
                 }
-
-                else if (e.key.code == Keyboard::Up)        //Check if the other key pressed is UP key
-                {
-                    rotate = true;
-                }                        		   //Rotation gets on
+            }
+        }*/ 
+if (!pausegame){
+        //---Event Listening Part---//
+    
+        while (window.pollEvent(e)){                    //Event is occurring - until the game is in running state
+            if (e.type == Event::Closed)                   //If cross/close is clicked/pressed
+                window.close();                            //Opened window disposes
+            if (e.type == Event::KeyPressed) {             //If any other key (not cross) is pressed
+                if (e.key.code == Keyboard::Up)            //Check if the other key pressed is UP key
+                    rotate = true;                         //Rotation gets on
                 else if (e.key.code == Keyboard::Left)     //Check if the other key pressed is LEFT key
                     delta_x = -1;                          //Change in X-Axis - Negative
                 else if (e.key.code == Keyboard::Right)    //Check if the other key pressed is RIGHT key
-                    delta_x = 1;                           //Change in X-Axis - Positive
-                else if (e.key.code == Keyboard::H)	   //opens the menu window
-                {
-                    bool choice = true;
-                    choice = menu(timer, menubg, level, delay, score);	   //menu function
-                }
-                else if (Keyboard::isKeyPressed(Keyboard::Space)) //When space is pressed delay is 0 and the sprite falls down
-                {
-
-                    delay = 0.0001;
-                }
+                    delta_x = 1;                           //Change in X-Axis - Positive  
+                    
             }
         }
-
-
-
-
-
-
+        if (Keyboard::isKeyPressed(Keyboard::Down))   //Just another way to detect key presses without event listener
+            delay=0.05;                                    //If DOWN key is pressed, delay in falling piece decreases from 0 to 0.05
 
         ///////////////////////////////////////////////
         ///*** START CALLING YOUR FUNCTIONS HERE ***///
-        if (bombbool == false)
-            fallingPiece(timer, delay, colorNum);                 //Example: fallingPiece() function is called here
-        else
-            bombbool = colorbomb(timer, delay, colorNum, bombbool, var);//when bomb will be true will run the bomb ftn
-        lineclear = line_clearing(score);//clears line
-        if (lineclear) //when line clears will return true
-            sound.play(); //when lineclear bool true sound will play
-        movement(delta_x);//move blocks
-        if (rotate)
-            rotate = rotate();//get rotation of blocks
-        gamestate = game_end();//to check if the game is teriminated or not after the blocks are filled
-
+        
+         move(delta_x);   
+       fallingPiece(timer, delay , colorNum); 
+     rotation(rotate);
+       disappear();             //Example: fallingPiece() function is called here
         ///*** YOUR CALLING STATEMENTS END HERE ***///
         //////////////////////////////////////////////
 
-
+delta_x=0;
         window.clear(Color::Black);
         window.draw(background);
-        if (bombbool == false)
-        {
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-
-                    if (gameGrid[i][j] == 0)
-                        continue;
-                    sprite.setTextureRect(IntRect(gameGrid[i][j] * 18, 0, 18, 18));
-                    sprite.setPosition(j * 18, i * 18);
-                    sprite.move(28, 31); 
-                    window.draw(sprite);
-                }
-            }
-            for (int i = 0; i < 4; i++) {
-                sprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-                sprite.setPosition(point_1[i][0] * 18, point_1[i][1] * 18);
-                sprite.move(28, 31);
+        for (int i=0; i<M; i++){
+            for (int j=0; j<N; j++){
+                if (gameGrid[i][j]==0)
+                    continue;
+                sprite.setTextureRect(IntRect(gameGrid[i][j]*18,0,18,18));
+                sprite.setPosition(j*18,i*18);
+                sprite.move(28,31); //offset
                 window.draw(sprite);
             }
         }
-        else
-        {
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-
-                    if (gameGrid[i][j] == 0)
-                        continue;
-                    bomb.setTextureRect(IntRect(gameGrid[i][j] * 18, 0, 18, 18));
-                    bomb.setPosition(j * 18, i * 18);
-                    bomb.move(28, 31); 
-                    window.draw(bomb);
-                }
-            }
-            {
-                bomb.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-                bomb.setPosition(point_1[0][0] * 18, point_1[0][1] * 18);
-                bomb.move(28, 31);
-                window.draw(bomb);
-            }
-        }
-
-
-        
+        for (int i=0; i<4; i++){
+            sprite.setTextureRect(IntRect(colorNum*18,0,18,18));
+            sprite.setPosition(point_1[i][0]*18,point_1[i][1]*18);
+            sprite.move(28,31);
+            window.draw(sprite);
+          }
+        //---The Final on Which Everything is Drawn Over is Loaded---//
         window.draw(frame);
-        Text text1, text2;
-        Font font;
-       
-     
-
-
-        delta_x = 0;
-        time = 0;
-        reset(level, delay);
-        
-        if (!anamoly() || gamestate)
-        {
-            window.close();
-        }
+        //---The Window that now Contains the Frame is Displayed---//
+        window.display();
     }
+    else {
+            // Game is paused
+            window.draw(pauseScreen);
+            window.draw(pausedText);
+            window.draw(resumeText);
+            window.draw(quitText);
+            window.display();
 
-    RenderWindow game_over(VideoMode(320, 480), "GAME OVER");
-    Text text1, text2;
-    Font font;
-   
-    text1.setFont(font);
-    text1.setFillColor(Color::Red);
-    text1.setCharacterSize(30);
-    text1.setString("GAME OVER");
-    text1.setPosition(window.getSize().x / 3, 10.f);
-    int ysize = window.getSize().y / 3;
-
-
-    Clock clock1;
-    while (game_over.isOpen())
-    {
-
-        float time1 = clock1.getElapsedTime().asSeconds();
-        bool select = false;
-        clock1.restart();
-       
-        Event a;
-        while (game_over.pollEvent(a))
-        {
-            if (a.type == Event::Closed)
-                game_over.close();
-
-        }
-
-        game_over.clear(Color::Black);
-        game_over.draw(menubg);
-        game_over.draw(text1);
-       
-        game_over.display();
- 
+            bool resumeselected = true;
+            while (pausegame) {
+                Event event;
+                while (window.pollEvent(event)) {
+                    if (event.type == Event::Closed) {
+                        window.close();
+                        pausegame = false;
+                    }
+                    else if (event.type == sf::Event::KeyPressed) {
+                        if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Down) {
+                            resumeselected = !resumeselected;
+                            if (resumeselected) {
+                                resumeText.setFillColor(sf::Color::Red);
+                                quitText.setFillColor(sf::Color::White);
+                            }
+                            else {
+                                resumeText.setFillColor(sf::Color::White);
+                                quitText.setFillColor(sf::Color::Red);
+                            }
+                        }
+                        else if (event.key.code == sf::Keyboard::Enter) {
+                            if (resumeselected) {
+                                pausegame = false;
+                            }
+                            else {
+                                window.close();
+                                pausegame = false;
+                            }
+                        }
+                    }
+                }
+            
     }
-
-
-
-
+    }
+    }
     return 0;
 }
+
+
